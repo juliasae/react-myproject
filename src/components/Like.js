ad677c645b310f85f0a321e0c2bedbe9f32dd4b8
juliasae <124267115+juliasae@users.noreply.github.com>
2023-03-21 14:26:44 +0100

    likes

--- src/components/Like.js
+++ src/components/Like.js
@@ -4,14 +4,14 @@ const Like = () =>{
     const handleclick = () => {
         alert("Glad att du gillar oss!")
     }
-        return(
-            <div class="button1">
-                <button
-                 onClick={handleclick}
-                 >Jag gillar Hemnet👍🏼</button>
-            </div> 
+    return(
+        <div class="button1">
+            <button onClick={handleclick}>
+                Jag gillar Hemnet👍🏼
+            </button>
+        </div> 
 
-        );
+    );
    
 }
 export default Like;
